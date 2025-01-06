50908032760478cbe27853c8f0916a6525980fc5
Park Joy <devpjoy@gmail.com>
2025-01-06 15:55:10 +0900

    start to solve the A001

--- A001_박조이_20240106.cpp
+++ A001_박조이_20240106.cpp
@@ -1,9 +1,28 @@
+//A001 : Assign Cookies (leetcode) - Easy
+/* 최대한 많은 아이들에게 만족할 만한 쿠키를 주어야 한다. 
+쿠키 사이즈(int[] g)와 아이들이 만족하는 쿠키 사이즈 (int[] s)가 매개변수로 주어지는 함수를 작성한다. */
+//적어도 하나의 쿠키는 주어야 한다.
+
+/*
+Input: g = [1,2,3], s = [1,1]
+Output: 1
+Explanation: You have 3 children and 2 cookies. The greed factors of 3 children are 1, 2, 3. 
+And even though you have 2 cookies, since their size is both 1, you could only make the child whose greed factor is 1 content.
+You need to output 1.
+*/
+
 #include <iostream> 
 
 using namespace std;
- 
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        
+    }
+};
+
 int main() 
 {
-    cout << "hello world" << endl;
     return 0; 
 }
