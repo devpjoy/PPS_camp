747c73313c1da9b48aafa80a17eb7239c5b42297
Park Joy <devpjoy@gmail.com>
2025-01-08 22:41:14 +0900

    complete A008 and create A009:

---
+++ A007_박조이_20250107.cpp
@@ -0,0 +1,35 @@
+/* 
+작성자 : 박조이 (22200313)
+문제 번호 : A007
+문제 제목 : 음계 판별하기 (by 백준)
+문제 설명 : 8개의 숫자를 입력받고 이 숫자들이 오름차순 정렬일 경우 ascending, 내림차순 정렬일 경우 descending, 
+        이 둘이 아니라면 mixed를 출력한다.
+문제 난이도 : Very Easy
+소요 시간 : 00시간 00분 
+*/
+
+/* 
+다장조는 c d e f g a b C, 총 8개 음으로 이루어져있다. 이 문제에서 8개 음은 다음과 같이 숫자로 바꾸어 표현한다. c는 1로, d는 2로, ..., C를 8로 바꾼다.
+1부터 8까지 차례대로 연주한다면 ascending, 8부터 1까지 차례대로 연주한다면 descending, 둘 다 아니라면 mixed 이다.
+연주한 순서가 주어졌을 때, 이것이 ascending인지, descending인지, 아니면 mixed인지 판별하는 프로그램을 작성하시오
+
+입력
+첫째 줄에 8개 숫자가 주어진다. 이 숫자는 문제 설명에서 설명한 음이며, 1부터 8까지 숫자가 한 번씩 등장한다.
+
+출력
+첫째 줄에 ascending, descending, mixed 중 하나를 출력한다.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+bool solution(string s)
+{
+
+}
+
+int main() {
+   
+    return 0; 
+}
---
+++ A008_박조이_20250107.cpp
@@ -0,0 +1,71 @@
+/* 
+작성자 : 박조이 (22200313)
+문제 번호 : A008
+문제 제목 : 평균은 넘겠지 (by 백준)
+문제 설명 : 테스트 케이스(학생들의 점수)를 입력 받고 케이스마다 한 줄씩 평균을 넘는 학생들의 비율을 반올림하여
+        소수점 셋째 자리까지 출력한다.
+문제 난이도 : Very Easy
+소요 시간 : 01시간 10분 
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int test_num;
+    cin >> test_num;
+
+    int people;
+    int grade;
+    double sum = 0;
+
+    vector<vector<int>> v(test_num);
+    vector<float> average;
+
+    //입력 받음
+    for (int i = 0; i < test_num; i ++) {
+        cin >> people;
+        v[i].push_back(people);
+
+        for (int j = 0; j < people; j ++) {
+            cin >> grade;
+            v[i].push_back(grade);
+        }
+    }
+
+    //평균 구하기 
+    for (int i = 0; i < test_num; i ++) {
+        for (int j = 1; j <= v[i][0]; j ++) {
+            sum += v[i][j];
+        }
+        average.push_back(sum / v[i][0]);
+        sum = 0;
+    }
+
+    //평균을 넘는 학생들의 수 구하기
+    vector<float> great_people(test_num, 0);
+
+    for (int i = 0; i < test_num; i++) {
+        for (int j = 1; j <= v[i][0]; j++) { 
+            if (v[i][j] > average[i]) {
+                great_people[i]++;
+            }   
+        }
+    }
+
+    //평균을 넘는 학생들의 비율을 구하기
+    for (int i = 0; i < great_people.size(); i ++) {
+        great_people[i] = great_people[i] / v[i][0] * 100.0;
+    }
+
+    cout << "\n\n";
+
+    //출력
+    for (int i = 0; i < great_people.size(); i ++) {
+        printf("%.3f\n", great_people[i]);
+    }
+
+    return 0; 
+}
