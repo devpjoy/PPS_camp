cf01eafb32270267db56d2f0cc7d7bbb73245155
Park Joy <devpjoy@gmail.com>
2025-01-13 16:05:47 +0900

    A031 complete

---
+++ A031_박조이_20250109.cpp
@@ -0,0 +1,23 @@
+/* 
+작성자 : 박조이 (22200313) 
+문제 번호 : A031
+문제 제목 : 초콜릿 자르기 (by 백준)
+문제 난이도 : Very Easy
+소요 시간 : 00시간 06분 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int n;
+    int m;
+    cin >> n >> m;
+
+    int sum = 0;
+    sum = n * (m - 1) + (n - 1);
+
+    cout << sum << endl;
+    return 0;
+}
