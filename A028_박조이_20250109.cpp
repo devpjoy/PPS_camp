28139753ae3eee6a3af944e60f8b0cd457293cf1
Park Joy <devpjoy@gmail.com>
2025-01-13 15:11:27 +0900

    complete

--- A028_박조이_20250109.cpp
+++ A028_박조이_20250109.cpp
@@ -3,39 +3,23 @@
 문제 번호 : A028
 문제 제목 : 큰 수 A + B (by 백준)
 문제 난이도 : Very Easy
-소요 시간 : 00시간 00분 
+소요 시간 : 00시간 05분 
 */
 
-#include <string>
-#include <vector>
 #include <iostream>
 
 using namespace std;
 
-bool solution(int x) {
-    bool answer = true;
-    string x1 = to_string(x);
-
-    int num = x1.length();
-    int origin_x = x;
-    int sum = 0;
-
-    for (int i = x1.length() - 1; i >= 0; i --) {
-        sum += x / pow(10, i);
-        x = x % (int)(pow(10, i));
-    }
-
-    if (origin_x % sum != 0) {
-        answer = !answer;
-    }
-
-    return answer;
-}
-
 int main () {
-    int x = 11;
-    if (solution(x)) cout << "true" << endl;
-    else cout << "false" << endl;
-
+    unsigned long long int a;
+    unsigned long int b;
+    cin >> a;
+    cin >> b;
+    cout << a << endl;
+    cout << b << endl;
+    
+    unsigned long long int sum = 0;
+    sum = a + b;
+    cout << sum << endl;
     return 0;
 }
