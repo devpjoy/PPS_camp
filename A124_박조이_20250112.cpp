11442a9814b44f59a7bd8e3f8650929417f9417a
Park Joy <devpjoy@gmail.com>
2025-01-14 18:15:31 +0900

    complete

---
+++ A124_박조이_20250112.cpp
@@ -0,0 +1,28 @@
+/* 
+작성자 : 박조이 (22200313) 
+문제 번호 : A124
+문제 제목 : 소인수분해 (by 백준)
+문제 난이도 : Very Easy
+소요 시간 : 00시간 00분 
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main () {
+    int n;
+    cin >> n;
+
+    while (n != 1) {
+        for (int i = 2; i <= n; i ++) {
+            while (n % i == 0) { //i로 나누어 떨어지는 동안에 
+                n /= i;
+                cout << i << endl;
+            }
+        }
+    }
+    
+    return 0;
+}
