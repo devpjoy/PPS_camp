d4e178950a956a72795bfb69b7540b67f5a2cf57
Park Joy <devpjoy@gmail.com>
2025-01-14 18:05:21 +0900

    complete:

---
+++ A123_박조이_20250112.cpp
@@ -0,0 +1,40 @@
+/* 
+작성자 : 박조이 (22200313) 
+문제 번호 : A123
+문제 제목 : 1로 만들기 (by 백준)
+문제 난이도 : Very Easy
+소요 시간 : 00시간 25분 
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main () {
+    int x;
+    cin >> x;
+
+    int count = 0;
+
+    while (x != 1) {
+        if (x % 3 == 0) {
+            x /= 3;
+            count ++;
+        }
+
+        else if (x % 3 == 1) {
+            x --;
+            count ++;
+            x /= 3;
+            count ++;
+        }
+
+        else if (x % 2 == 0) {
+            x /= 2;
+            count ++;
+        }
+    }
+
+    cout << count << endl;
+}
